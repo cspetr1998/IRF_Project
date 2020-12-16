e4c941e2ceef26cdc3b02ee2851b45c69f5ef85a
Csesznok Petronella <cspetra1998@gmail.com>
2020-12-16 17:54:44 +0100

    Button osztály létrehozása

---
+++ IRF_Project/IRF_Project/Entities/ButtonClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project.Entities
+{
+    class ButtonClass : Button
+    {
+        public ButtonClass()
+        {
+            this.Text = "Hozzáadás";
+            this.Width = 100;
+            this.Height = 50;
+            this.BackColor = System.Drawing.Color.LightGreen;
+        }
+    }
+}
