069ef591d500b5974e027a85bcd1b98a065e0ece
Csesznok Petronella <cspetra1998@gmail.com>
2020-12-16 17:35:05 +0100

    Series tulajdonság megváltoztatása

--- IRF_Project/IRF_Project/Form1.Designer.cs
+++ IRF_Project/IRF_Project/Form1.Designer.cs
@@ -67,7 +67,7 @@
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
-            series1.Name = "Series1";
+            series1.Name = "Adatok";
             this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(423, 306);
             this.chart1.TabIndex = 2;
