aca656c6d13ad92b4f72234826b9614d8d9f3c89
Csesznok Petronella <cspetra1998@gmail.com>
2020-12-16 17:27:31 +0100

    Rendeles osztály létrehozása

---
+++ IRF_Project/IRF_Project/Entities/Rendeles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_Project.Entities
+{
+    class Rendeles
+    {
+        public string TortaNev { get; set; }
+        public int Allergen { get; set; }
+    }
+}
