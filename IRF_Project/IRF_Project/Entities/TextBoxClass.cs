db86b503b53d38f45d93f68577f7137a93847cf6
Csesznok Petronella <cspetra1998@gmail.com>
2020-12-16 17:53:39 +0100

    TextBox osztály létrehozása

---
+++ IRF_Project/IRF_Project/Entities/TextBoxClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_Project.Entities
+{
+    public class TextBoxClass : TextBox
+
+    {
+        public TextBoxClass()
+        {
+            this.Text = "Sütemény neve...";
+            this.GotFocus += RemoveText;
+            this.Width = 200;
+        }
+
+        public void RemoveText(object sender, EventArgs e)
+        {
+            if (this.Text == "Sütemény neve...")
+            {
+                this.Text = "";
+            }
+        }
+        public void AddText(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(this.Text))
+            {
+                this.Text = "Sütemény neve...";
+            }
+        }
+        public String getText()
+        {
+            return this.Text == "Sütemény neve..." ? "" : this.Text;
+        }
+    }
+
+}
